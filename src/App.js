7135c4e6222e9b7385c98228d1d2aabc8a8f929d
karthik <karthik98@gmail.com>
2024-06-24 17:06:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import '../src/assets/stylesheets/abstracts/global.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Born To Achieve Great Things!</p>
+    </div>
+  );
+}
+
+export default App;
